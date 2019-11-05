7d1cdf1d223a9c9e11fe9b6bd8ef5f4170ab73ee
panchepanevski <pance.panevski@gmail.com>
2019-11-05 15:48:56 +0100

    start with  js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const firstInput = document.querySelector("[name=first]");
+const secondInput = document.querySelector("name=second");
+const submitButton = document.querySelector("name=first");
+
+const sum = parseInt(firstInput.value) + parseInt(secondElement.value);
+alert(sum);
