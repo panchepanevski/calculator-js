1d289aeecfeed9bbe7f27d72a3137e8977f81c97
panchepanevski <pance.panevski@gmail.com>
2019-11-06 16:54:46 +0100

    add count digits function

--- src/lib/operators.js
+++ src/lib/operators.js
@@ -9,3 +9,7 @@ export function divide(first, second) {
 export function multiply(first, second) {
   return first * second;
 }
+
+export function countDigits(number) {
+  return number.toString().length;
+}
